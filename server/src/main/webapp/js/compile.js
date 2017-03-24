0e3815417403f9064e34a16d286ea958f1377312
Allan Delporte <allan.delporte@etudiant.univ-lille1.fr>
2017-03-24 10:00:19 +0100

    generer tableau

--- server/src/main/webapp/js/compile.js
+++ server/src/main/webapp/js/compile.js
@@ -1,27 +1,27 @@
-function compile(div_editor, div_button){
-        var editor = ace.edit(div_editor);
-        $(document).ready(function() {
+function compile(div_editor, div_button) {
+    var editor = ace.edit(div_editor);
+    $(document).ready(function () {
 
-            $("#"+div_button).click(function() {
-                console.log(editor.getValue());
-                var url = "../v1/exercice";
-                console.log("postUserGeneric " + url)
-                $.ajax({
-                    type: 'POST',
-                    contentType: 'application/json',
-                    url: url,
-                    dataType : "json",
-                    data: JSON.stringify({
-                        "code": editor.getValue()
-                    }),
-                    success: function(data, textStatus, jqXHR) {
-                        console.log(data);
-                    },
-                    error: function(jqXHR, textStatus, errorThrown) {
-                        console.log('postUser error: ' + textStatus);
-                    }
-                });
-            })
+        $("#" + div_button).click(function () {
+            console.log(editor.getValue());
+            var url = "../v1/exercice";
+            console.log("postUserGeneric " + url)
+            $.ajax({
+                type: 'POST',
+                contentType: 'application/json',
+                url: url,
+                dataType: "json",
+                data: JSON.stringify({
+                    "code": editor.getValue()
+                }),
+                success: function (data, textStatus, jqXHR) {
+                    console.log(data);
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    console.log('postUser error: ' + textStatus);
+                }
+            });
+        })
 
-        });
+    });
 }
