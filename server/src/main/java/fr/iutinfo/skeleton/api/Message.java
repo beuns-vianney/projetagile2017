95be6815056840b9bbae34dd701eae3f6ba765e8
Vianney BEUNS <vianney.beuns@etudiant.univ-lille1.fr>
2017-03-24 09:33:25 +0100

    Serveur fonctionnel

---
+++ server/src/main/java/fr/iutinfo/skeleton/api/Message.java
@@ -0,0 +1,21 @@
+package fr.iutinfo.skeleton.api;
+
+public class Message {
+
+	private String retour;
+	
+	public Message() {
+		
+	}
+
+	public String getRetour() {
+		return retour;
+	}
+
+	public void setRetour(String retour) {
+		this.retour = retour;
+	}
+	
+	
+	
+}
