9e564899449a9e0e3b2bffae45287fe2f042dd65
Meftouh <meftouh.r@gmail.com>
2017-03-27 14:49:58 +0200

    Modification

--- server/src/main/java/fr/iutinfo/skeleton/api/UtilisateurRessource.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/UtilisateurRessource.java
@@ -1,10 +1,10 @@
 package fr.iutinfo.skeleton.api;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 @Path("/utilisateur")
@@ -15,13 +15,13 @@ public class UtilisateurRessource {
 	@GET
 	@Path("/connexion")
 	@Produces("application/json")
-	public Utilisateur connection(@FormParam("identifiant") String compte, @FormParam("mdp") String mdp) {
+	public Utilisateur connection(@QueryParam("identifiant") String compte, @QueryParam("mdp") String mdp) {
 		Utilisateur user = null;
 		MethodeGitApi mtgapi;
 		try{
-			mtgapi =new MethodeGitApi(compte,mdp);
+			mtgapi =new MethodeGitApi(compte,mdp); 
 			
-			user = RequeteBDD.usersByToken(mtgapi.getPrivate_token());
+			user = RequeteBDD.usersByToken(mtgapi.getPrivateToken());
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
