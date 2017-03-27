97e3729a27f344002a71dec90a661ba03fec62a0
Meftouh <meftouh.r@gmail.com>
2017-03-27 15:25:29 +0200

    Connexion soon bon

--- server/src/main/java/fr/iutinfo/skeleton/api/UtilisateurRessource.java
+++ server/src/main/java/fr/iutinfo/skeleton/api/UtilisateurRessource.java
@@ -1,27 +1,40 @@
 package fr.iutinfo.skeleton.api;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+
+import org.gitlab4j.api.models.Session;
 
 @Path("/utilisateur")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class UtilisateurRessource {
     
-	@GET
+	@POST
 	@Path("/connexion")
-	@Produces("application/json")
-	public Utilisateur connection(@QueryParam("identifiant") String compte, @QueryParam("mdp") String mdp) {
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	public Utilisateur connection(@FormParam("identifiant") String compte, @FormParam("mdp") String mdp) {
+		System.out.println("================> PAR ICI");
 		Utilisateur user = null;
 		MethodeGitApi mtgapi;
 		try{
-			mtgapi =new MethodeGitApi(compte,mdp); 
-			
-			user = RequeteBDD.usersByToken(mtgapi.getPrivateToken());
+			System.out.println("COMPTE: "+compte);
+			mtgapi = new MethodeGitApi(); 
+			Session session = mtgapi.login(compte, mdp);
+			System.out.println("================> PAR ICI 2");
+			if (session != null) {
+				user = RequeteBDD.usersByToken(mtgapi.getPrivateToken());
+				if (user == null) {
+					RequeteBDD.insert(session.getEmail().split(".")[1].split("@")[0], session.getEmail().split(".")[0], 1, 1, session.getPrivateToken());
+				}
+				System.out.println(user);
+				System.out.println("================> PAR ICI 3");
+			} else
+				return null;
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 		}
