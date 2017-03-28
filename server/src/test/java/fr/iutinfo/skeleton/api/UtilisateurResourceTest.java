ffa74995b1fe42fb1850f7842761c05f131238d8
Maxime PICAULT <maxime.picault@etudiant.univ-lille1.fr>
2017-03-28 11:39:47 +0200

    gitlab

---
+++ server/src/test/java/fr/iutinfo/skeleton/api/UtilisateurResourceTest.java
@@ -0,0 +1,25 @@
+package fr.iutinfo.skeleton.api;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class UtilisateurResourceTest {
+
+	/*@Test
+	public void should_find_user_by_token () {
+		UserDao userDao = BDDFactory.getDbi().open(UserDao.class);
+		userDao.dropUserTable();
+		userDao.createUserTable();
+		
+		User belsa = new User();
+		belsa.setLogin("belsa");
+		String token = "123456789";
+		belsa.setToken(token);
+		userDao.insertUser(belsa);
+		
+		User user  = userDao.findByToken(token);
+		Assert.assertEquals("belsa",user.getLogin());
+	}*/
+	
+}
